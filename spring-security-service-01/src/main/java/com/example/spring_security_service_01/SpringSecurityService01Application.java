1893ee264cc7452cdef83f764d17d8c7a1c4e3c7
fmahadyBD <fmahady01@gmail.com>
2025-04-30 12:21:10 +0600

    Initial commit

---
+++ spring-security-service-01/src/main/java/com/example/spring_security_service_01/SpringSecurityService01Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_service_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityService01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityService01Application.class, args);
+	}
+
+}
---
+++ spring-security-service-01/src/test/java/com/example/spring_security_service_01/SpringSecurityService01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_security_service_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityService01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

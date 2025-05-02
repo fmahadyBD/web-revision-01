5c1aaed844dee50bd4350c86e7223ba3d7854963
fmahadyBD <fmahady01@gmail.com>
2025-05-02 10:31:59 +0600

    adding annotion in main menthod

--- spring-security-service-01/src/main/java/com/example/spring_security_service_01/SpringSecurityService01Application.java
+++ spring-security-service-01/src/main/java/com/example/spring_security_service_01/SpringSecurityService01Application.java
@@ -1,13 +1,32 @@
 package com.example.spring_security_service_01;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+import com.example.spring_security_service_01.user.Role;
+import com.example.spring_security_service_01.user.RoleRepository;
+
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+@EnableAsync
 @SpringBootApplication
 public class SpringSecurityService01Application {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityService01Application.class, args);
 	}
+
+	@Bean
+public CommandLineRunner runner(RoleRepository roleRepository){
+	return args ->{
+		if(roleRepository.findByName("USER").isEmpty()){
+			roleRepository.save(Role.builder().name("USER").build());
+		}
+	};
+}
 
 }
--- spring-security-service-01/src/main/java/com/example/spring_security_service_01/user/RoleRepository.java
+++ spring-security-service-01/src/main/java/com/example/spring_security_service_01/user/RoleRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<Integer,Role>{
+public interface RoleRepository extends JpaRepository<Role,Integer>{
     
     Optional<Role> findByName(String name);
 }
